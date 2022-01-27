9cffb9237325728a4a27b521f275cbd78c32bb9c
Erika Jones <38572406+erikajns@users.noreply.github.com>
2022-01-27 11:21:00 -0600

    GitHub SPA config

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
+    path: '/portfolio/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/portfolio/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
@@ -16,7 +21,7 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/experience',
+    path: '/portfolio/experience',
     name: 'Experience',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
