817c8ad2c98754284e60c27961255c3beaaadc61
Erika Jones <38572406+erikajns@users.noreply.github.com>
2022-01-28 17:52:45 -0600

    Projects Page Initial
    Part One

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,15 +1,125 @@
 <template>
-    <div>
-        PROJECTS
-    </div>
+  <div class="wrapper">
+    <Flicking :options="{ renderOnlyVisible: true }">
+      <div
+        v-for="(project, index) in projects"
+        class="flicking-panel has-background-primary"
+        :key="index"
+      >
+        <div class="project-image">
+          <img :src="`${publicPath}${project.image}`" />
+        </div>
+        <div class="project-detail">
+          <h1>{{ project.title }}</h1>
+          <p class="project-detail__date">Made on: {{ project.date }}</p>
+          <div class="project-detail__description">
+          <h3>Description</h3>
+          <p>{{ project.description }}</p>
+          <p><b>Next Updates:</b> {{ project.updates }}</p>
+          </div>
+          <a class="project-detail__button" :href="project.url">See project</a>
+        </div>
+      </div>
+    </Flicking>
+  </div>
 </template>
 
 <script>
-    export default {
-        name:"projects-page"
-    }
+import Flicking from "@egjs/vue3-flicking";
+import projectData from "./../data/projects.json";
+
+export default {
+  name: "projects-page",
+  components: {
+    Flicking: Flicking,
+  },
+  data() {
+    return {
+      projects: projectData,
+      publicPath: process.env.BASE_URL,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
+@import "./../variables.scss";
+@import url("./../../node_modules/@egjs/vue-flicking/dist/flicking.css");
 
+.flicking-index {
+  min-width: 60px;
+  position: relative;
+  text-align: center;
+}
+
+.flicking-camera {
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  position: relative;
+  width: 100%;
+  z-index: 1;
+}
+
+.flicking-panel {
+  display: flex;
+  flex-direction: column;
+  max-width: 90vw;
+  text-align: left;
+  justify-content: center;
+  border: 4px dotted $pink;
+  border-radius: 5px;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  margin-right: 10px;
+  padding: 30px 20px;
+
+  .project-image {
+    max-width: 100%;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .project-detail {
+    h1 {
+      margin-bottom: 0;
+    }
+    &__date {
+      margin-bottom: 30px;
+      margin-top: 0;
+      font-size: 14px;
+      color: rgb(141, 141, 141);
+    }
+
+    &__description {
+      margin-bottom: 65px;
+    }
+  }
+}
+
+@media (min-width: map-get($grid-breakpoints, "md")) {
+  .flicking-panel {
+    .project-image {
+      max-width: 350px;
+    }
+  }
+}
+
+@media (min-width: map-get($grid-breakpoints, "lg")) {
+  .flicking-panel {
+    flex-direction: row;
+    .project-detail {
+      flex: 1;
+      max-width: 50%;
+    }
+    .project-image {
+      flex: 1;
+      max-width: 40%;
+      margin-top: 10%;
+      margin-right: 50px;
+    }
+  }
+}
 </style>
